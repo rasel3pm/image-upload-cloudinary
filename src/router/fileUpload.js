463364d064d6a4313c8aa070f705b8fd2cc34c8b
Rasel <mdraselkibria11@gmail.com>
2023-06-19 18:33:36 +0600

    update image upload system

--- src/router/fileUpload.js
+++ src/router/fileUpload.js
@@ -13,25 +13,27 @@ cloudinary.config({
   api_secret: process.env.API_SECRATE,
 });
 
-router.post("/image", upload.single("file"), (req, res) => {
-  try {
-    const image = req.file;
-    if (image) {
-      // Upload the image to Cloudinary
-      cloudinary.uploader.upload(image.path, (err, result) => {
-        if (err) {
-          return res.json(err);
-        } else {
-          res.status(200).json({
-            url: result.url,
-            public_id: result.public_id,
-          });
-        }
+router.post("/upload", upload.single("image"), (req, res) => {
+  // Check if file exists
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  // Upload image to Cloudinary
+  cloudinary.uploader.upload(
+    req.file.path,
+    { folder: "uploads" }, // Optional: specify a folder in Cloudinary to store the image
+    (error, result) => {
+      if (error) {
+        return res.status(500).json({ error: "Image upload failed" });
+      }
+      // Image uploaded successfully
+      res.status(200).json({
+        message: "Image Upload success",
+        url: result.secure_url,
+        public_id: result.public_id,
       });
     }
-    res.json({ message: "file upload success" });
-  } catch (error) {
-    res.json({ message: "file upload faild" });
-  }
+  );
 });
+
 module.exports = router;
